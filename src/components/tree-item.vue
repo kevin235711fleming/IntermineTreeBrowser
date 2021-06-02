2417d7a4a89428db6b3860180818d6c65a04ddb3
Kevin Fleming <kevin.fleming@cruk.cam.ac.uk>
2021-06-02 16:01:34 +0100

    only process and render the tree node children on the toggle event to avoid too much recursion

--- src/components/tree-item.vue
+++ src/components/tree-item.vue
@@ -7,12 +7,13 @@
                 [{{ showChildren ? '-' : '+' }}]
             </span>
         </div>
-    <ul v-show="showChildren || rootNode" v-if="item.isTreeNode || rootNode">
+    <ul v-show="showChildren || root" v-if="showNode() || root">
         <tree-item
-         v-for="child in item"
+         v-for="child in makeSortedTree"
          :key="child.name"
          :item="child"
-         :rootNode=false>
+         :treeData="treeData"
+         :root=false>
         </tree-item>
     </ul>
   </li>
@@ -23,7 +24,8 @@ export default {
     name: 'tree-item',
     props: {
         item: Object,
-        rootNode: Boolean
+        treeData: Object,
+        root: Boolean
     },
     data: function() {
         return {
@@ -32,11 +34,32 @@ export default {
     },
     methods: {
         toggle: function() {
-          if (this.isNode) {
+          if (this.item.isTreeNode) {
             this.showChildren = !this.showChildren;
           }
+        },
+        showNode: function() {
+            return this.item.isTreeNode && this.showChildren
         }
-    }
+    },
+    computed: {
+        sortedClassAttributes () {
+          return Object.fromEntries(Object.entries(this.item.attributes).sort())
+        },
+        makeSortedTree () {
+            let result = []
+              let leafAttributes = (this.root) ? Object.fromEntries(Object.entries(this.item.attributes).sort()) 
+                                            : Object.fromEntries(Object.entries(this.treeData[this.item.referencedType].attributes).sort())
+              let classNodes = (this.root) ? Object.fromEntries(Object.entries({...this.item.collections, ...this.item.references}).sort()) 
+                                            : Object.fromEntries(Object.entries({...this.treeData[this.item.referencedType].collections, ...this.treeData[this.item.referencedType].references}).sort())
+              Object.keys(leafAttributes).forEach(objectKey => result.push(leafAttributes[objectKey]))
+              Object.keys(classNodes).forEach(objectKey => {
+                classNodes[objectKey].isTreeNode = true
+                result.push(classNodes[objectKey])
+              })
+              return result
+        }
+  }
 }
 
 </script>
